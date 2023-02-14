841256ba16f1ca0725492f947a819194624034eb
Just-Midhun <113001139+Just-Midhun@users.noreply.github.com>
2023-02-14 12:01:23 +0530

    Create palindrome.asm

---
+++ 8086/palindrome.asm
@@ -0,0 +1,50 @@
+ASSUME CS: CODE,DS:DATA
+
+DATA SEGMENT
+STRING DB 'alla$'
+STRING1 DB 'String is palindrome$'
+STRING2 DB 'String is not palindrome$'
+DATA ENDS 
+
+CODE SEGMENT
+START:
+
+MOV AX, DATA
+ MOV DS, AX  
+ 
+ MOV SI,OFFSET STRING
+ LOOP1 :
+    MOV AX, [SI]
+    CMP AL, '$'
+    JE LABEL1
+    INC SI
+    JMP LOOP1
+ LABEL1 :
+    MOV DI,OFFSET STRING
+    DEC SI
+
+    LOOP2 :
+     CMP SI, DI
+     JL OUTPUT1
+     MOV AX,[SI]
+     MOV BX, [DI]
+     CMP AL, BL
+     JNE OUTPUT2
+ 
+    DEC SI
+    INC DI
+    JMP LOOP2
+ 
+ OUTPUT1:
+    LEA DX,STRING1
+    MOV AH, 09H
+    INT 21H
+    JMP L1
+ OUTPUT2:
+    LEA DX,STRING2
+    MOV AH,09H
+    INT 21H
+L1: MOV AH, 4CH
+ INT 21H
+CODE ENDS
+END START
